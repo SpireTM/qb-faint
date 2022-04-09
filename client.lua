b26dbc230e13e557e434781130ae20dff78edd98
SpireTM <48876949+SpireTM@users.noreply.github.com>
2022-04-09 14:11:50 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,30 +1,30 @@
-QBCore = exports['qb-core']:GetSharedObject()
+QBCore = exports['qb-core']:GetCoreObject()
 
 local isRagdolling = false
 
 RegisterCommand('faint', function()
-	QBCore.Functions.GetPlayerData(function(PlayerData)
+    QBCore.Functions.GetPlayerData(function(PlayerData)
         if not PlayerData.metadata["isdead"] and not PlayerData.metadata["inlaststand"] and not IsPauseMenuActive() then
-			isRagdolling = not isRagdolling
-			if isRagdolling then
-				QBCore.Functions.Notify("you lost consciousness!")
-				exports['qb-drawtext']:DrawText('[U] - Get up', 'left')	
-			else
-				exports['qb-drawtext']:HideText()
-			end
-			RagdollLoop()
-		end
-	end)
+            isRagdolling = not isRagdolling
+            if isRagdolling then
+                QBCore.Functions.Notify("You lost consciousness!")
+                exports['qb-core']:DrawText('[U] - Get up', 'left')
+            else
+                exports['qb-core']:HideText()
+            end
+            RagdollLoop()
+        end
+    end)
 end)
 
 RegisterKeyMapping('faint', 'Lose consciousness', 'keyboard', 'U')
 
 
 function RagdollLoop()
-	Citizen.CreateThread(function()
-		while isRagdolling do
-			Citizen.Wait(0)
-			SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, 0, 0, 0)
-		end
-	end)
+    Citizen.CreateThread(function()
+        while isRagdolling do
+            Citizen.Wait(0)
+            SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, 0, 0, 0)
+        end
+    end)
 end
